1494f34aef500b55f9dfa48f0aca463d0c488e34
hanuniye <hanuniye@gmail.com>
2024-09-22 10:46:53 +0300

    woking on homepage

---
+++ src/components/Add.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Add = ({ size }: { size: "sm" | "md" | "lg" }) => {
+  return <div className="p-4 bg-white shadow-md rounded-lg text-sm"></div>;
+};
+
+export default Add;
